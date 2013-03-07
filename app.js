dc1961f6c8616422919d18ee8d27eb6231343a0f
Brian Partridge <bpartridge@Brians-MacBook-Air-2.local>
2013-03-07 14:04:56 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+	app = express(),
+	http = require('http'),
+	server = http.createServer(app);
+
+var port_range = [5000, 5009];
+	
+app.get('/status', function (req, res) {
+	
+	var pjson = require('./package.json');
+	
+	return res.send({
+		status: 'running',
+		environment: app.get('site'),
+		version: pjson.version
+	});
+	
+});
+	
+var portscanner = require('portscanner');
+
+portscanner.findAPortNotInUse(port_range[0], port_range[1], 'localhost', function(error, _port) {
+
+	var port = argv.port || process.env.PORT || _port;
+
+	server.listen(port, function () {
+		
+		console.log('Listening on Port ' + port);
+		
+	});
+
+});
